4bfeb335b08ea3adb171a6c03a75637b0f7aa8a1
Dan <105542989+4aniDev@users.noreply.github.com>
2023-01-28 21:00:27 +0500

    Create architecture layers

--- app/src/main/java/ru/chani/wallpapersetter/presentation/MainActivity.kt
+++ app/src/main/java/ru/chani/wallpapersetter/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.chani.wallpapersetter
+package ru.chani.wallpapersetter.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.chani.wallpapersetter.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
