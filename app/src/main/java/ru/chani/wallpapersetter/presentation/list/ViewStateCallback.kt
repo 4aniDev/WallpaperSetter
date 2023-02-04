70f49f15faa1b6a5280c8f031e57cddcbbf92b34
Dan <105542989+4aniDev@users.noreply.github.com>
2023-02-04 15:15:53 +0500

    Catch Network Error in Items of ImageList

---
+++ app/src/main/java/ru/chani/wallpapersetter/presentation/list/ViewStateCallback.kt
@@ -0,0 +1,18 @@
+package ru.chani.wallpapersetter.presentation.list
+
+import com.squareup.picasso.Callback
+import java.lang.Exception
+
+class ViewStateCallback(
+    private val holder: ImageListRvAdapter.CustomViewHolder,
+    private val onSuccess: (holder: ImageListRvAdapter.CustomViewHolder) -> Unit,
+    private val onError: (holder: ImageListRvAdapter.CustomViewHolder) -> Unit
+): Callback {
+    override fun onSuccess() {
+        onSuccess.invoke(holder)
+    }
+
+    override fun onError(e: Exception?) {
+        onError.invoke(holder)
+    }
+}
